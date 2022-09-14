f400c8eee46664275049e30df5beceb1eb897217
Dorothy <arianadottie@gmail.com>
2022-09-14 05:07:08 -0700

    print two tw digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+
+{
+	int first;
+	int last;
+
+	for (first = 0; first <= 98; first++)
+
+	{
+		for (last = last + 1; last <= 99; last++)
+		{
+
+		putchar((first / 10) + '0');
+		putchar((first % 10) + '0');
+		putchar(' ');
+		putchar((last / 10) + '0');
+		putchar((last % 10) + '0');
+
+
+		if (first == 98 && last == 99)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+
+		}
+
+	}
+
+putchar('\n');
+return (0);
+
+}
